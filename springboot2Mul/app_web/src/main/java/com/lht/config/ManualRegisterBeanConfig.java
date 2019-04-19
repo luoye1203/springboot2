2b8ef200d6c120362d6f316087688a1da50c854a
李红涛 <373309891@qq.com>
2019-04-19 17:24:23 +0800

    测试更改动态数据源代码

--- springboot2Mul/app_web/src/main/java/com/lht/config/ManualRegisterBeanConfig.java
+++ springboot2Mul/app_web/src/main/java/com/lht/config/ManualRegisterBeanConfig.java
@@ -7,6 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+
+/**
+ * lht
+ * 2019-4-19 17:24:00
+ *
+ */
+
 @Configuration
 @Import(DynamicDataSourceRegister.class)
 public class ManualRegisterBeanConfig {
