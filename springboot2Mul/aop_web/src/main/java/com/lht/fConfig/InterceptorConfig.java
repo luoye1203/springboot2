3852fbc1bf1ba98dc3b8dbfa71263005ba6683b4
李红涛 <373309891@qq.com>
2019-04-24 16:54:24 +0800

    添加拦截器

--- springboot2Mul/aop_web/src/main/java/com/lht/fConfig/InterceptorConfig.java
+++ springboot2Mul/aop_web/src/main/java/com/lht/fConfig/InterceptorConfig.java
@@ -1,14 +1,29 @@
 package com.lht.fConfig;
 
+import com.lht.eInterceptor.InterceptorMyA;
+import com.lht.eInterceptor.InterceptorMyB;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @Component
 @ConfigurationProperties("interceptor")
 @Data
-public class InterceptorConfig {
+public class InterceptorConfig implements WebMvcConfigurer {
    private List<String> urls;
+
+   @Override
+   public void addInterceptors(InterceptorRegistry registry) {
+      //        String[] urls1={"/interceptor/*"};
+      registry.addInterceptor(new InterceptorMyA()).addPathPatterns(urls.toArray(new String[urls.size()]));
+//        registry.addInterceptor(new InterceptorMyA()).addPathPatterns(urls);
+      registry.addInterceptor(new InterceptorMyB()).addPathPatterns(urls.toArray(new String[urls.size()]));
+//        registry.addInterceptor(new InterceptorMyB()).addPathPatterns(urls);
+
+   }
 }
