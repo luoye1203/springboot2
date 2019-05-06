fbe1ca4852f166bf6310cda0c16599c8667c09f8
lihongtao <373309891@qq.com>
2019-05-06 10:30:53 +0800

    去掉无效代码

--- springboot2Mul/websocket_web/src/main/java/com/lht/WebSocketStartApplication.java
+++ springboot2Mul/websocket_web/src/main/java/com/lht/WebSocketStartApplication.java
@@ -1,13 +1,10 @@
 package com.lht;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude={HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class WebSocketStartApplication extends SpringBootServletInitializer {
--- springboot2Mul/websocket_web/src/main/resrouces/static/publicJs.js
+++ springboot2Mul/websocket_web/src/main/resrouces/static/publicJs.js
@@ -1,7 +1,7 @@
 var ip="localhost";
 // var ip="10.4.106.24";
 var port="8080";
-var appName="messagePush_server";
+// var appName="messagePush_server";
 var uri="chat";
 
 // var wsurl="ws://"+ip+":"+port+"/"+appName+"/"+uri;
