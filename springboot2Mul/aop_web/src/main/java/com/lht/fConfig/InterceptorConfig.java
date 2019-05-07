271f1b01d13029919e481831f39381249c10632e
lihongtao <373309891@qq.com>
2019-05-07 13:56:27 +0800

    去除无效代码

--- springboot2Mul/aop_web/src/main/java/com/lht/fConfig/InterceptorConfig.java
+++ springboot2Mul/aop_web/src/main/java/com/lht/fConfig/InterceptorConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
