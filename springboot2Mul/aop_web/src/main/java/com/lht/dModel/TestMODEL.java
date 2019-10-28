72c1ae1a5ad070d08f3770b5b6554ded445fa465
李红涛 <373309891@qq.com>
2019-10-28 20:15:54 +0800

    增加websocket模块

---
+++ springboot2Mul/aop_web/src/main/java/com/lht/dModel/TestMODEL.java
@@ -0,0 +1,11 @@
+package com.lht.dModel;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
+public class TestMODEL {
+
+    @ApiModelProperty
+    private String id;
+}
