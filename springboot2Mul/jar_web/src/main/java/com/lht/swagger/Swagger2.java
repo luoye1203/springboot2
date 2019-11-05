e6685f429f8eb8df16e2ae42689fe1f05e2ff520
李红涛 <373309891@qq.com>
2019-11-05 15:37:02 +0800

    swagger改造

--- springboot2Mul/jar_web/src/main/java/com/lht/swagger/Swagger2.java
+++ springboot2Mul/jar_web/src/main/java/com/lht/swagger/Swagger2.java
@@ -1,6 +1,5 @@
 package com.lht.swagger;
 
-import com.lht.bController.DmJdbcController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
